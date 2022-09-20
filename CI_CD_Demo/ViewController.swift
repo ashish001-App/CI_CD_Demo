f8b1360f87d18651db4b29d72588b21da20b89dd
Ashish sisodia <ashish.sisodia@appinventiv.com>
2022-09-20 11:52:06 +0530

    add some message

--- CI_CD_Demo/ViewController.swift
+++ CI_CD_Demo/ViewController.swift
@@ -5,7 +5,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello! This is my first CI/CD project. ")
-        
+        print("2 Hello! This is my first CI/CD project. ")
+        print("2 Hello! This is my first CI/CD project. ")
+        print("2 Hello! This is my first CI/CD project. ")
         print("2 Hello! This is my first CI/CD project. ")
     }
 
